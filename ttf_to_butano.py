30bc0461297a6ad184da005bb1c9ace14f2bcf05
Gabriel Kwa <118412402+KwaGabriel@users.noreply.github.com>
2025-08-06 22:41:16 +0800

    New features! added more parameters

--- ttf_to_butano.py
+++ ttf_to_butano.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import chardet
 import codecs
 import os
@@ -32,6 +32,11 @@ while (ptf == ""):
     print("Path to font file:")
     ptf = input()
 
+tile_size = 0
+while (tile_size <= 0):
+    print("Tile size:")
+    tile_size = int(input())
+
 font_size = 0
 while (font_size <= 0):
     print("Font size:")
@@ -47,6 +52,23 @@ while (spacing < 0):
     print("Character spacing:")
     spacing = int(input())
 
+font_yoffset = -1
+while (font_yoffset < 0):
+    print("Y-offset (for weird fonts):")
+    font_yoffset = int(input())
+
+bg_fill = ""
+while ((bg_fill != "Y".casefold()) and (bg_fill != "N".casefold())):
+    print("Fill BG (Y/N):")
+    bg_fill = input().casefold()
+
+bg_fill = bg_fill.casefold() == "Y".casefold()
+
+drop_shadow = -1
+while (drop_shadow < 0):
+    print("Drop Shadow Offset:")
+    drop_shadow = int(input())
+
 this_path = ptf.split("/")
 filename = this_path.pop(-1).replace(".ttf", "").replace(" ","_").replace("-","_")
 
@@ -59,9 +81,9 @@ font_path = ptf
 font = ImageFont.truetype(font_path, font_size)
 if(font == None):
     sys.exit("Font not found, exiting. Path specified: " + font_path)
+
 text_to_render = []
 
-text_color = (0,0,0)
 invalid_utf8 = [127,129,141,143,144,157,160,173]
 character_ascii_data = []
 
@@ -75,31 +97,53 @@ for i in range(127,256):
     if(i not in invalid_utf8):
         sprite_chars += fix_and_convert_str_to_utf8(chr(i)) + "\n"
 
-image_width = font_size 
-image_height = len(text_to_render) * font_size - font_size # for the blank ' ' character
+image_width = tile_size 
+image_height = len(text_to_render) * tile_size - tile_size # for the blank ' ' character
 
 image = Image.new("P", (image_width, image_height), "#7f007f")
 image_palette = image.getpalette()
 if(image_palette != None): #put second color into palette, white.
+    image_palette.insert(3,0)
+    image_palette.insert(3,0)
+    image_palette.insert(3,0)
     image_palette.insert(3,255)
     image_palette.insert(3,255)
     image_palette.insert(3,255)
+
+    if bg_fill:
+        image_palette.insert(3,127)
+        image_palette.insert(3,127)
+        image_palette.insert(3,0)
+
     image.putpalette(image_palette)
 
-draw = ImageDraw.Draw(image)
+blank_tile = Image.new("P", (tile_size, tile_size), "#7f007f")
+if(image_palette != None):
+    blank_tile.putpalette(image_palette)
+draw = ImageDraw.Draw(Image.new("P",(0,0)))
 
 if(antialiased == "Y"):
     antialiased = "L" #antialiased, while set to 1 for aliased
 else:
     antialiased = "1"
-draw.fontmode = antialiased
 
 y_position = 0
-for i in range(1,len(text_to_render)):
-    y_position = (i - 1) * font_size # Move down by font_size for the next character
-    draw.text((0, y_position), text_to_render[i], font=font, spacing=spacing, align="left", fill=text_color)
+text_color_white = (255,255,255)
+text_color_black = (0,0,0)
+for i in range(1,len(text_to_render)):    
+    y_position = (i-1) * tile_size
+    this_tile = blank_tile.copy()
+    draw = ImageDraw.Draw(this_tile)
+    draw.fontmode = antialiased
+    if drop_shadow != 0:
+        for x in range(drop_shadow - 1, 0, -1):
+            draw.text((x, font_yoffset + x), text_to_render[i], font=font, spacing=spacing, align="left", fill=text_color_black)
+        draw.text((0, font_yoffset), text_to_render[i], font=font, spacing=spacing, align="left", fill=text_color_white)
+    else:
+        draw.text((0, font_yoffset), text_to_render[i], font=font, spacing=spacing, align="left", fill=text_color_black)
 
-blank_tile = Image.new("P", (font_size, font_size), "#7f007f")
+    image.paste(this_tile, (0, y_position))
+
 if(image_palette != None):
     blank_tile.putpalette(image_palette)
 
@@ -109,20 +153,20 @@ for char_index in range(33, 256): #33 is because relevant ascii characters start
     if (char_index) in invalid_utf8:
         continue
 
-    i = (ascii_index - 1) * font_size
+    i = (ascii_index - 1) * tile_size
     left = 0
     upper = i
-    right = font_size
-    lower = i + font_size
+    right = tile_size
+    lower = i + tile_size
     
-    tile = image.crop((left, upper, right, lower)).convert('P', palette=Image.Palette.ADAPTIVE, colors=3)
+    tile = image.crop((left, upper, right, lower)).convert('P', palette=Image.Palette.ADAPTIVE, colors=4)
     width, height = tile.size
     # Iterate through each row and then each pixel to find consecutive runs
     valid_pixels = 0
-    empty_pixels_on_left = font_size
+    empty_pixels_on_left = tile_size
 
     for y in range(height):
-        current_consecutive_length = font_size
+        current_consecutive_length = tile_size
         for right_x in range(width,0,-1):
             pixel_color = tile.getpixel((right_x - 1, y))
 
@@ -143,13 +187,17 @@ for char_index in range(33, 256): #33 is because relevant ascii characters start
                 break
         empty_pixels_on_left = min(empty_pixels_on_left, current_consecutive_length)
 
-    for y in range(height):
-        for x in range(width):
-            if(x - spacing >= valid_pixels):
-                break
-            if tile.getpixel((x,y)) == 0:
-                tile.putpixel((x,y), (255,255,255)) #put to second color in the palette, aka white
-    
+    if bg_fill:
+        valid_pixels -= 1 #to get calculations on base zero
+        tile = ImageOps.expand(tile, border=(0,0,spacing,0), fill="#7f007f")
+        for y in range(height):
+            for x in range(width + spacing):
+                if(x > valid_pixels + spacing):
+                    break
+                if tile.getpixel((x,y)) == 0:
+                    tile.putpixel((x,y), (0,127,127))
+        valid_pixels += 1
+
     image.paste(blank_tile,(0, i))
     image.paste(tile, (-empty_pixels_on_left, i))
     character_size = valid_pixels - empty_pixels_on_left + spacing
